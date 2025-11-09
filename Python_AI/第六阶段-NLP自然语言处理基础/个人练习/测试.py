22d9bc1ca97f1bdd6a646189ddf992fe5c453ed6
zlq <1739708098@qq.com>
2025-11-09 18:35:10 +0800

    std提交1

--- Python_AI/第六阶段-NLP自然语言处理基础/个人练习/测试.py
+++ Python_AI/第六阶段-NLP自然语言处理基础/个人练习/测试.py
@@ -13,8 +13,4 @@ class Embedding(nn.Module):
     def forward(self, x):
         return self.luc
         std
-        这是第一次提交
-        这是第二次提交
-        这是第三次提交
-        这是第四次提交
-        这是第五次提交
+        1
